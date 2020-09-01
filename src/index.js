aaae51ca28e63cddfd052dc38f665b969f2e6523
yuyaebean <yuyaebean@gmail.com>
2020-09-01 23:35:59 +0900

    React의 4가지 개념 - Components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+function FCHello() {
+  return (
+    <>
+      <h1>Hello Functional Component!</h1>
+    </>
+  );
+}
+
+class CCHello extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello Class Component!</h1>
+      </>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
-    const name = "Tarrazu";
     return (
-      <div>
-        <h1 className="title">Hello {name}!</h1>
-      </div>
+      <>
+        <FCHello />
+        <CCHello />
+      </>
     );
   }
 }
