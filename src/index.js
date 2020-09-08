84d12e3658ea3bc85a6ed05a52ab7c19c92c7ae0
yuyaebean <yuyaebean@gmail.com>
2020-09-08 23:56:34 +0900

    React의 4가지 개념 - state

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ReactDom from "react-dom";
 
 function FCHello({ name }) {
+  const [number, setNumber] = useState(0);
+
   return (
     <>
       <h1>Hello {name}!</h1>
+      <h2>{number}</h2>
+      <button
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        Plus
+      </button>
     </>
   );
 }
 
 class CCHello extends Component {
+  state = {
+    number: 0,
+  };
+
   render() {
     const { name } = this.props;
     return (
       <>
         <h1>Hello {name}!</h1>
+        <h2>{this.state.number}</h2>
+        <button
+          onClick={() => {
+            this.setState({ number: this.state.number + 1 });
+          }}
+        >
+          Plus
+        </button>
       </>
     );
   }
